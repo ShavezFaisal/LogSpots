87846e24eeedaa2620e0326f868b3d9b2455c6e4
shavez <shavez@SHAIZ>
2017-05-21 14:03:53 -0700

    Add project files.

---
+++ LogSpots/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LogSpots.Startup))]
+namespace LogSpots
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
