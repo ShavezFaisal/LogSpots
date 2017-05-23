ce5c47f5053432ebb87aa50bbf79f355a3a1a93f
Cori 5 <Cori 5@HP>
2017-05-23 19:40:04 +0500

    yes ho gaya

--- LogSpots/App_Start/IdentityConfig.cs
+++ LogSpots/App_Start/IdentityConfig.cs
@@ -59,7 +59,7 @@ namespace LogSpots
                 RequireLowercase = false,
                 RequireUppercase = false,
             };
-
+            //aho hi gaya
             // Configure user lockout defaults
             manager.UserLockoutEnabledByDefault = true;
             manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
