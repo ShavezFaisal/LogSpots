dd3c7b7454c910d537625fd825ac5a61872963cc
shavez <shavez@SHAIZ>
2017-05-25 13:47:33 -0700

    upload videos

--- LogSpots/Controllers/HomeController.cs
+++ LogSpots/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,6 +26,52 @@ namespace LogSpots.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult upload()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult upload(HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //Method 1 Get file details from current request    
+                    //Uncomment following code if you wants to use method 1    
+                    //if (Request.Files.Count > 0)    
+                    // {    
+                    //     var Inputfile = Request.Files[0];    
+
+                    //     if (Inputfile != null && Inputfile.ContentLength > 0)    
+                    //     {    
+                    //         var filename = Path.GetFileName(Inputfile.FileName);    
+                    //       var path = Path.Combine(Server.MapPath("~/uploadedfile/"), filename);    
+                    //        Inputfile.SaveAs(path);    
+                    //    }    
+                    // }    
+
+                    //Method 2 Get file details from HttpPostedFileBase class    
+
+                    if (file != null)
+                    {
+                        string path = Path.Combine(Server.MapPath("~/UploadedVideos"), Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+
+                    }
+                    ViewBag.FileStatus = "File uploaded successfully.";
+                }
+                catch (Exception)
+                {
+
+                    ViewBag.FileStatus = "Error while file uploading.";
+                }
+
+            }
+            return View("upload");
         }
     }
 }
